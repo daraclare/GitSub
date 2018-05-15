995cb4b6fd3d5b74e4e92e6ee820d0ba52ab236d
Dara Carolan <dara.carolan@accenture.com>
2018-05-15 11:04:58 +0100

    add name, bio and location

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 const inputValue = document.querySelector("#searchInput");
 const searchButton = document.querySelector(".searchButton");
+const pictureDiv = document.querySelector(".profile-picture");
+const article = document.querySelector("article");
+const profilePicture = document.createElement("img");
+const usernameDiv = document.createElement("h2");
+const locationDiv = document.createElement("h3");
+const bioDiv = document.createElement("p");
+
 const CLIENTID = "Iv1.e1ad797ab8151ab0";
 const CLIENTSECRET = "b9b29dcad95e9cabf6f4dbf01715bb40936d3900";
 const BASE = "https://api.github.com/users/";
@@ -8,15 +15,24 @@ const fetchUsers = async user => {
   const URL = `${BASE}${user}?client_id=${CLIENTID}&client_secret=${CLIENTSECRET}`;
   let fetchAPI = await fetch(URL);
   let data = await fetchAPI.json();
-  return { data };
+  return data;
 };
 
 const showData = () => {
   fetchUsers(inputValue.value)
     .then(res => {
       console.log(res);
+      profilePicture.setAttribute("src", res.avatar_url);
+      pictureDiv.appendChild(profilePicture);
+      usernameDiv.textContent = res.name;
+      article.appendChild(usernameDiv);
+      locationDiv.textContent = res.location;
+      article.appendChild(locationDiv);
+      bioDiv.textContent = res.bio;
+      article.appendChild(bioDiv);
     })
     .catch(err => {
+      // eslint-disable-next-line
       console.log(err);
     });
 };
